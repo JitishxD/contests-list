83231d758dc44557b8ce9ff674c0d21386df1e99
JitishxD <jitish08@gmail.com>
2025-12-18 13:53:37 +0530

    refactor

--- src/manifest.js
+++ src/manifest.js
@@ -1,5 +1,5 @@
 import { defineManifest } from '@crxjs/vite-plugin'
-import packageData from '../package.json' assert { type: 'json' }
+import packageData from '../package.json' with { type: 'json' }
 
 const isDev = process.env.NODE_ENV == 'development'
 
@@ -15,11 +15,10 @@ export default defineManifest({
     128: 'img/logo-128.png',
   },
   action: {
-    default_popup: 'popup.html',
+    default_popup: 'src/popup/popup.html',
     default_icon: 'img/logo-48.png',
   },
-  options_page: 'options.html',
-  devtools_page: 'devtools.html',
+  options_page: 'src/options/options.html',
   background: {
     service_worker: 'src/background/index.js',
     type: 'module',
@@ -30,17 +29,11 @@ export default defineManifest({
       js: ['src/contentScript/index.js'],
     },
   ],
-  side_panel: {
-    default_path: 'sidepanel.html',
-  },
   web_accessible_resources: [
     {
       resources: ['img/logo-16.png', 'img/logo-32.png', 'img/logo-48.png', 'img/logo-128.png'],
       matches: [],
     },
   ],
-  permissions: ['sidePanel', 'storage'],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
+  permissions: ['storage'],
 })
